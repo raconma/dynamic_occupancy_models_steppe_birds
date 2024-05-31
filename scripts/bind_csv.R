9605fb057525b058a9ea7ae18f2ce0a47beb5a98
raconma <raconma@gmail.com>
2024-05-31 12:04:12 +0200

    Separate scripts and data, and modified the scripts so

--- scripts/bind_csv.R
+++ scripts/bind_csv.R
@@ -13,8 +13,8 @@ especies <- list(
 for (i in seq_along(especies)) {
 
   # Read the two files that we want to merge
-  df1 <- read.csv(paste0("data/",especies[[i]][2],"/ebd_",especies[[i]][2],"_breeding_spain_zf_part1_variables.csv"))
-  df2 <- read.csv(paste0("data/",especies[[i]][2],"/ebd_",especies[[i]][2],"_breeding_spain_zf_part2_variables.csv"))
+  df1 <- read.csv(paste0("../data/",especies[[i]][2],"/ebd_",especies[[i]][2],"_breeding_spain_zf_part1_variables.csv"))
+  df2 <- read.csv(paste0("../data/",especies[[i]][2],"/ebd_",especies[[i]][2],"_breeding_spain_zf_part2_variables.csv"))
   
   # Merge both files
   df_bind <- rbind(df1, df2)
@@ -23,5 +23,5 @@ for (i in seq_along(especies)) {
   df_bind <- df_bind[, -ncol(df_bind)]
   
   # Save the merged file
-  write.csv(df_bind, file = paste0("data/",especies[[i]][2],"/ebd_", especies[[i]][2], "_breeding_spain_variables.csv"), row.names = FALSE)
+  write.csv(df_bind, file = paste0("../data/",especies[[i]][2],"/ebd_", especies[[i]][2], "_breeding_spain_variables.csv"), row.names = FALSE)
 }
